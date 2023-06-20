99670b613b3a396a1eed83f81883257d5b736b7e
Salvatore Parisi <salvatoreparisi97@gmail.com>
2023-06-20 18:38:16 +0200

    Esercizio 17 svolto.

---
+++ src/House.java
@@ -0,0 +1,41 @@
+public class House {
+
+    private int floorsNumber;
+   private String address;
+
+    private int residentsNumber;
+    private String[] residentsNames;
+
+
+    //set array length of residentNames for add names ** FIRST STEP
+    public void setResidentNumber(int residentNumber) {
+        this.residentsNumber = residentNumber;
+        this.residentsNames = new String[residentNumber];
+    }
+
+    // get/setter residentNames
+    public String[] getResidentsNames() {
+        return residentsNames;
+    }
+    public void setResidentsNames(int index, String name) {
+        if (index >= 0 && index < residentsNames.length) {
+            residentsNames[index] = name;
+        }
+    }
+
+    //get/setter floorNumbers
+    public int getFloorsNumber() {
+        return floorsNumber;
+    }
+    public void setFloorsNumber(int floorsNumber) {
+        this.floorsNumber = floorsNumber;
+    }
+
+    // get/setter address.
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,56 @@
+/*ESERCIZIO 17
+Achieve encapsulation using getters/setters for a class House that has the following instance variables:
+  - an int floorsNumber
+  - a string address
+  - an array of strings residentsNames
+use a tester class to:
+  - set the values by asking the user to input them
+  - for the residentsNames, the user will have to input a list of names separated by a comma (e.g. John,Lucy,Peter)
+  - prints the house details using the getters and String.format()
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String nameResidentsList;
+
+        House house1 = new House();
+
+        // soluzione per settare le variabili dell'oggetto, da tastiera.
+
+        System.out.println("Please, enter the floors number: ");
+
+        house1.setFloorsNumber(input.nextInt());
+        input.nextLine();
+
+        System.out.println("Please, enter the address:");
+        house1.setAddress(input.nextLine());
+
+        System.out.println("Please, enter the residents number: ");
+        house1.setResidentNumber(input.nextInt());
+        input.nextLine();
+
+// soluzuione per scrivere e salvare i nomi dei residenti, in base alla lunghezza del metodo getResidentsName
+        System.out.println("Please, enter the residents names: ");
+
+        for (int i = 0; i < house1.getResidentsNames().length; i++) {
+            house1.setResidentsNames(i, input.nextLine());
+        }
+
+        System.out.println();
+
+// soluzione per stampare i dettagli della casa nel formato qui sotto:
+        nameResidentsList= String.join(", ",(house1.getResidentsNames()));
+
+        //formato
+        String details = String.format("Details:\nfloors number: %d;\nAddress: %s; \nName residents: %s.",
+                                        house1.getFloorsNumber(), house1.getAddress(), nameResidentsList);
+
+        System.out.println(details);
+
+
+    }
+}
